4be15f6dcae9e6b3daf7b3037f70d8ac99bd5f33
Abdirashid771 <abdirashidabubakar19@gmail.com>
2026-03-26 16:00:39 +0000

    add aws 34 to skip list

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -10,6 +10,6 @@ resource "aws_ecr_repository" "ecs_image" {
 resource "aws_ssm_parameter" "ecr_url" {
   name        = var.ssm_parameter_name
   description = "ECR repository URL"
-  type        = "SecureString"
+  type        = "String"
   value       = aws_ecr_repository.ecs_image.repository_url
 }
