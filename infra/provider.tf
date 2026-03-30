203ec4a7e9ca05c1a6c4cbc4e0fce24cb0aa487c
Abdirashid771 <abdirashidabubakar19@gmail.com>
2026-03-30 21:15:14 +0100

    add dynamo-db lock

--- infra/provider.tf
+++ infra/provider.tf
@@ -13,9 +13,11 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket  = "ecs-project-terraform-state-0"
-    key     = "ecs-project-terraform-locks"
-    region  = "eu-west-2"
-    encrypt = true
+    bucket         = "ecs-project-terraform-state-0"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+    dynamodb_table = "ecs-project-terraform-locks"
   }
 }
+
